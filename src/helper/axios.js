4e44f827f80dcc8a3a18e085018d2f639c502253
Attah David <jocheattahdavid@gmail.com>
2020-08-27 12:51:20 +0100

    Add Movies started

---
+++ src/helper/axios.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+// this is tge base url o mke request to the movie database
---
+++ src/helper/request.js
@@ -0,0 +1,17 @@
+const API_KEY= "807ba2a3cf0352caf1d66354c4c053e6";
+
+
+// https://api.themoviedb.org/3
+
+const request ={
+    fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, 
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`, 
+    fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`, 
+    fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`, 
+    fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`, 
+    fetchDocumentaries: `/discover/tv?api_key=${API_KEY}&with_genres=99`, 
+}
+
+export default request;
